3cef470473ff985373df75f9da66c9dc722c83c9
jenith <jenithleon@live.com>
2016-07-16 19:09:15 +0530

    developmentchanges

--- app/src/main/java/com/adosat/adosat/EEEMatLab.java
+++ app/src/main/java/com/adosat/adosat/EEEMatLab.java
@@ -1,10 +1,12 @@
 package com.adosat.adosat;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EEEMatLab extends Fragment {
@@ -27,8 +29,16 @@ public class EEEMatLab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_eee_mat_lab, container, false);
-
-
+        View root =  inflater.inflate(R.layout.activity_eee_mat_lab, container, false);
+        Button btn1 = (Button) root.findViewById(R.id.bsylla);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),SttadProActivity.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
+
 }
---
+++ app/src/main/java/com/adosat/adosat/MatlabActivity.java
@@ -0,0 +1,26 @@
+package com.adosat.adosat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by jenith on 7/16/2016.
+ */
+
+public class  MatlabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matlab);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+    }
+}
