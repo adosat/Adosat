4fe11764fb2b5ef4019ed65e3b4b5c3a92185f4d
jenith <jenithleon@live.com>
2016-07-18 10:00:35 +0530

    developmentchanges

--- app/src/main/java/com/adosat/adosat/CrossPlatformActivity.java
+++ app/src/main/java/com/adosat/adosat/CrossPlatformActivity.java
@@ -3,14 +3,24 @@ package com.adosat.adosat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 /**
  * Created by jenith on 7/15/2016.
  */
 public class CrossPlatformActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crossplatform);
 
+        mWebView = (WebView) findViewById(R.id.web4);
+
+        String text = "<html><body>" + "<p align=\"justify\">"
+                + getString(R.string.aboutus) + "</p> " + "</body></html>";
+
+        mWebView.loadData(text, "text/html", "utf-8");
     }
 }
